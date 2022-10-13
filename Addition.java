58048bde4d290916a893aa3c755883909d260fff
march2299 <javabatchmar22@gmail.com>
2022-10-13 16:14:08 +0530

    Addition program

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition {
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        int c= a+b;
+        System.out.println("a = "+ a);
+        System.out.println("b = "+b);
+        System.out.println("Addition = "+c);
+    }
+}
